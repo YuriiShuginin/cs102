6a11828b0ac2e25cd13d4d4712a51d2fd7ddf85d
Yurii <yurii.shuginin@yandex.ru>
2019-10-11 21:26:29 +0300

    Реализована функция поиска НОД

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -21,6 +21,27 @@ def is_prime(n: int) -> bool:
     return boo
     pass
 
+def gcd(a: int, b: int) -> int:
+    """
+    >>> gcd(12, 15)
+    3
+    >>> gcd(3, 7)
+    1
+    """
+    # PUT YOUR CODE HERE
+
+    g = 0
+    if a > b:
+        for i in range(1, b+1):
+            if (a % i == 0) and (b % i == 0):
+                g = i
+    elif a < b:
+        for i in range(1, a+1):
+            if (a % i == 0) and (b % i == 0):
+                g = i
+    else: g = a
+    return g
+    pass
 
 def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
     if not (is_prime(p) and is_prime(q)):
@@ -28,11 +49,8 @@ def generate_keypair(p: int, q: int) -> Tuple[Tuple[int, int], Tuple[int, int]]:
     elif p == q:
         raise ValueError('p and q cannot be equal')
 
-    # n = pq
-    # PUT YOUR CODE HERE
-
-    # phi = (p-1)(q-1)
-    # PUT YOUR CODE HERE
+    n = p * q
+    phi = (p - 1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
