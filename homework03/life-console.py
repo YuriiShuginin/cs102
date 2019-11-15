c07c442a5e8fecbd7700e481c53732f196aeb654
Yurii <yurii.shuginin@yandex.ru>
2019-11-15 22:42:24 +0300

    Добавлены финальные версии

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -1,43 +1,48 @@
 import curses
-import time
 
 from life import GameOfLife
 from ui import UI
 
 class Console(UI):
 
-    def __init__(self, life: GameOfLife) -> None:
+    def __init__(self, life: GameOfLife):
         super().__init__(life)
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        screen.border(0)
-    
+        screen.addstr(0, 0, "+")
+        screen.addstr(0, self.life.cols + 1, "+")
+        screen.addstr(self.life.rows + 1, 0, "+")
+        screen.addstr(self.life.rows + 1, self.life.cols + 1, "+")
+        for i in range(1, self.life.cols + 1):
+            screen.addstr(0, i, "-")
+        for i in range(1, self.life.cols + 1):
+            screen.addstr(self.life.rows + 1, i, "-")
+        for i in range(1, self.life.rows + 1):
+            screen.addstr(i, 0, "|")
+        for i in range(1, self.life.rows + 1):
+            screen.addstr(i, self.life.cols + 1, "|")
+
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        for row_ind, row in enumerate(self.life.curr_generation):
-            for col_ind, cell in enumerate(row):
-                if cell != 0:
-                    try:
-                        screen.addstr(row_ind + 1, col_ind + 1, '*')
-                    except Exception as e:
-                        print(row_ind, col_ind)
+        for i in range(self.life.rows):
+            for j in range(self.life.cols):
+                if self.life.curr_generation[i][j] == 0:
+                    screen.addstr(i + 1, j + 1, " ")
+                else:
+                    screen.addstr(i + 1, j + 1, "*")
 
     def run(self) -> None:
         screen = curses.initscr()
-        curses.curs_set(0)
-        running = True
-        while running:
-            screen.clear()
-            self.draw_borders(screen)
+        screen.clear()
+        self.draw_borders(screen)
+        screen.refresh()
+        while not self.life.is_max_generations_exceeded and self.life.is_changing:
             self.draw_grid(screen)
-            self.life.step()
-            if self.life.is_max_generations_exceeded:
-                running = False
             screen.refresh()
-            time.sleep(1 / 60)
-        screen.getch()
+            self.life.step()
         curses.endwin()
+
 
 if __name__ == '__main__':
     life = GameOfLife((24, 80), max_generations=50)
